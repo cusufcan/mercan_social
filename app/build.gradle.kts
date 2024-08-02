03e57b8113388d4322603cd0028babaf42f3478c
Yusuf Can Mercan <cusufcan@gmail.com>
2024-08-02 21:28:39 +0300

    firebase dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -48,4 +49,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Firebase
+    implementation(platform(libs.firebase.bom))
+    // Firebase Analytics
+    implementation(libs.firebase.analytics)
 }
